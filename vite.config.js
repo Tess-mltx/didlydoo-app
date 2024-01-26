825f54aed21d23db94d518c85f8f31f3daf78493
Tess <tess@DESKTOP-9F1581K>
2024-01-26 15:35:36 +0100

    correct config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,5 @@
 // vite.config.js
 export default {
   base: '/didlydoo-app/',
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  },
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      title: 'Page d\'accueil',
-    },
-    secondPage: {
-      entry: 'src/mainEvent.js',
-      template: 'public/event.html',
-      title: 'Deuxième Page',
-    },
-  },
+  
 };
